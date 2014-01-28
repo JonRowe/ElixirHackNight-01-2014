d7f6aa3985db5c4d1aa8b279a32635839d1ae5a9
Jon Rowe <hello@jonrowe.co.uk>
2014-01-28 21:45:37 +1100

    blinker examples

--- test/conway_test.exs
+++ test/conway_test.exs
@@ -87,4 +87,71 @@ defmodule ConwayTest do
                             ]
   end
 
+  test "blinker" do
+    states = [
+               [
+                 [false, false, false, false, false],
+                 [false, false, false, false, false],
+                 [false,  true,  true,  true, false],
+                 [false, false, false, false, false],
+                 [false, false, false, false, false],
+               ],
+               [
+                 [false, false, false, false, false],
+                 [false, false,  true, false, false],
+                 [false, false,  true, false, false],
+                 [false, false,  true, false, false],
+                 [false, false, false, false, false],
+               ]
+             ]
+    assert Conway.run(Enum.at(states,0)) == Enum.at(states,1)
+    assert Conway.run(Enum.at(states,1)) == Enum.at(states,0)
+  end
+
+  test "toad" do
+    states = [
+               [
+                 [false, false, false, false, false, false],
+                 [false, false, false,  true, false, false],
+                 [false,  true, false, false,  true, false],
+                 [false,  true, false, false,  true, false],
+                 [false, false,  true, false, false, false],
+                 [false, false, false, false, false, false],
+               ],
+               [
+                 [false, false, false, false, false, false],
+                 [false, false, false, false, false, false],
+                 [false, false,  true,  true,  true, false],
+                 [false,  true,  true,  true, false, false],
+                 [false, false, false, false, false, false],
+                 [false, false, false, false, false, false],
+               ]
+             ]
+    assert Conway.run(Enum.at(states,0)) == Enum.at(states,1)
+    assert Conway.run(Enum.at(states,1)) == Enum.at(states,0)
+  end
+
+  test "blinker" do
+    states = [
+               [
+                 [false, false, false, false, false, false],
+                 [false,  true,  true, false, false, false],
+                 [false,  true,  true, false, false, false],
+                 [false, false, false,  true,  true, false],
+                 [false, false, false,  true,  true, false],
+                 [false, false, false, false, false, false],
+               ],
+               [
+                 [false, false, false, false, false, false],
+                 [false,  true,  true, false, false, false],
+                 [false,  true, false, false, false, false],
+                 [false, false, false, false,  true, false],
+                 [false, false, false,  true,  true, false],
+                 [false, false, false, false, false, false],
+               ]
+             ]
+    assert Conway.run(Enum.at(states,0)) == Enum.at(states,1)
+    assert Conway.run(Enum.at(states,1)) == Enum.at(states,0)
+  end
+
 end
