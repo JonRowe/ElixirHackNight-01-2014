12855af73e6c206dd61c135a7e5566a35e665605
Jon Rowe <hello@jonrowe.co.uk>
2014-01-28 18:58:23 +1100

    isolate examples from wrapping

--- test/conway_test.exs
+++ test/conway_test.exs
@@ -9,12 +9,30 @@ defmodule ConwayTest do
   end
 
   test "A cell with fewer than two neighbours will die" do
-    new_board = Conway.run [[true,true],[false, false]]
-    assert new_board == [[false,false],[false,false]]
+    new_board = Conway.run [
+                             [false, false, false],
+                             [false,  true,  true],
+                             [false, false, false]
+                           ]
+    assert new_board ==    [
+                             [false, false, false],
+                             [false, false, false],
+                             [false, false, false]
+                            ]
   end
 
   test "A cell with two neighbours lives!" do
-    new_board = Conway.run [[true,true],[true, false]]
-    assert new_board == [[true,false],[false,false]]
+    new_board = Conway.run [
+                             [false, false, false],
+                             [false,  true, false],
+                             [false,  true,  true],
+                             [false, false, false]
+                           ]
+    assert new_board ==    [
+                             [false, false, false],
+                             [false,  true,  true],
+                             [false,  true,  true],
+                             [false, false, false]
+                            ]
   end
 end
